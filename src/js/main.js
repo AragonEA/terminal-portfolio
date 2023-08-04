ad1767cf6b4c27086577099ae2e8dcd99c17a752
Aragon Emanuel <aragon3m4@gmail.com>
2023-08-04 10:04:02 -0300

    Show a command on page when a modal is opened

--- src/js/main.js
+++ src/js/main.js
@@ -8,12 +8,32 @@ function initialize(){
 
 initialize();
 
+
+function logEvent(name){
+const $logs = document.querySelector('#logs');
+const container = document.createElement('article');
+
+const consoleUser = document.createElement('span');
+consoleUser.classList = 'console-user';
+consoleUser.textContent = '[visitor@linux-host ~]$';
+
+const logSpan = document.createElement('span')
+logSpan.classList = 'console-text';
+logSpan.textContent = ` modal -r ./${name}.txt`
+
+container.appendChild(consoleUser);
+container.appendChild(logSpan);
+$logs.appendChild(container);
+}
+
+
 function setUpModal(modalName) {
     const modal = document.querySelector(`#${modalName}Modal`)
     const btn = document.querySelector(`#${modalName}Btn`);
     const span = document.querySelector(`#${modalName}Span`);
 
     btn.onclick = function () {
+        logEvent(modalName);
         modal.style.display = "block";
     }
     span.onclick = function () {
